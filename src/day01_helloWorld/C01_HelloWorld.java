27f551fb6aab0cd4229d678050172bedf147de58
cennetatak <cennetatak1992@gmail.com>
2025-05-15 14:46:22 +0300

    ilk gönderim

---
+++ src/day01_helloWorld/C01_HelloWorld.java
@@ -0,0 +1,21 @@
+package day01_helloWorld;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+     // tek satırlık açıklama yazmak için // kullanabiliriz
+        System.out.println("Hello World");
+
+
+/*
+    birden fazla satır
+    açıklama yazmak için kullanılır.
+
+    java yorum satırlarının kendisi ile ilgili olmadığını bilir
+    ve çalıştırmaz
+
+    rengi de kodlardan farklı olarak gri'dir.
+ */
+    }
+}
