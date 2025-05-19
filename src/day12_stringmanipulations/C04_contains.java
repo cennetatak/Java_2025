1653bcb1f0661bd298e117ef369617ae91f96aa4
cennetatak <cennetatak1992@gmail.com>
2025-05-19 13:06:05 +0300

    12.gün

---
+++ src/day12_stringmanipulations/C04_contains.java
@@ -0,0 +1,26 @@
+package day12_stringmanipulations;
+
+public class C04_contains {
+    public static void main(String[] args) {
+
+        String str = "Java ogrenen pisman olmaz.";
+
+        System.out.println(str.contains("a")); // true
+
+        System.out.println( str.contains( "a o")  ); // true
+
+        System.out.println(str.contains("J    o")); // false
+
+        System.out.println(str.contains("Java og")); // true
+
+
+
+
+
+
+
+
+
+
+    }
+}
