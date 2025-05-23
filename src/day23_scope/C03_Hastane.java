514f377bb994673a69e21afb2d8d957ff3012554
cennetatak <cennetatak1992@gmail.com>
2025-05-23 09:49:36 +0300

    23.gün

---
+++ src/day23_scope/C03_Hastane.java
@@ -0,0 +1,20 @@
+package day23_scope;
+
+public class C03_Hastane {
+
+ /*
+        Class level'da olusturulan variable'lardan
+        TUM OBJELER icin ortak bir degere sahip olmasini istediklerimizi static,
+        HER BIR OBJE icin ayri bir degere sahip olmasini istediklerimizi instance olarak TANIMLAMALIYIZ
+     */
+
+    static String hastaneIsmi = "Yildiz Hastanesi";
+    static String hastaneAdresi = "Cankaya";
+    static String hastaneTelefon = "3122343434";
+    static String bashekim = "Dr Eda";
+
+    String persIsmi = "isim atanmadi";
+    String perAdres = "adres atanmadi";
+    String perTel = "tel atanmadi";
+
+}
